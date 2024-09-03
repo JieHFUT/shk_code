f049d5e7a5ae3b45a86e64079a3cb54659c653b4
JieCHERY <9594523wrj@gmail.com>
2024-09-03 15:27:52 +0800

    thread

---
+++ src/thread/testthreadsafe/SaleTicket1.java
@@ -0,0 +1,21 @@
+package thread.testthreadsafe;
+
+public class SaleTicket1 extends Thread{
+
+    static int sale = 0;
+    int ticketNum = 1000;
+    @Override
+    public void run() {
+        boolean flag = true;
+        while (flag) {
+            if (ticketNum > 0) {
+                System.out.println(Thread.currentThread().getName() + " 售票，票号为：" + ticketNum);
+                ticketNum--;
+                sale++;
+            }
+            if (ticketNum <= 0) {
+                flag = false;
+            }
+        }
+    }
+}
---
+++ src/thread/testthreadsafe/WindowTest1.java
@@ -0,0 +1,26 @@
+package thread.testthreadsafe;
+
+public class WindowTest1 {
+
+    public static void main(String[] args) {
+
+
+        SaleTicket1 saleTicket1 = new SaleTicket1();
+        saleTicket1.start();
+
+        SaleTicket1 saleTicket2 = new SaleTicket1();
+        saleTicket2.start();
+
+        SaleTicket1 saleTicket3 = new SaleTicket1();
+        saleTicket3.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("卖出去的票一共有: " + SaleTicket1.sale + " 张");
+
+
+    }
+}
