76bf641c66979623fcbe12efaae6dfcdf72f6c71
JieHFUT <9594523wrj@gmail.com>
2024-10-03 23:47:17 +0800

    test

---
+++ src/iotest/FileReaderWriter.java
@@ -0,0 +1,9 @@
+package iotest;/**
+* ClassName: FileReaderWriter
+* Package: iotest
+* Description: 
+* @Author jieHFUT
+* @Create 2024/9/20 23:05 
+* @Version 1.0   
+*/public class FileReaderWriter {
+}
