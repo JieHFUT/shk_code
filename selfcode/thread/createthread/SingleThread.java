d8c6d9a9208cb8d5d83dd80c6f8cf5464b49b50f
JieHFUT <9594523wrj@gmail.com>
2024-10-14 22:22:11 +0800

    thread

---
+++ selfcode/thread/createthread/SingleThread.java
@@ -0,0 +1,13 @@
+package thread.createthread;
+
+/**
+ * ClassName: SingleThread
+ * Package: thread.createthread
+ * Description:
+ *
+ * @Author jieHFUT
+ * @Create 2024/10/13 16:03
+ * @Version 1.0
+ */
+public class SingleThread {
+}
---
+++ selfcode/thread/threadtongbu/runnablesafe/Window.java
@@ -0,0 +1,21 @@
+package thread.threadtongbu.runnablesafe;
+
+/**
+ * ClassName: Wndow
+ * Package: thread.threadtongbu.runnablesafe
+ * Description:
+ *
+ * @Author jieHFUT
+ * @Create 2024/10/14 22:20
+ * @Version 1.0
+ */
+public class Window implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+}
