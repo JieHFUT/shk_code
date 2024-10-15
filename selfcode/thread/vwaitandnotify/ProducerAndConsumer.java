14e0072dbe838fd60a4a124cab1abd3276166781
JieHFUT <9594523wrj@gmail.com>
2024-10-16 07:27:24 +0800

    lockanddeadlock

--- selfcode/thread/vwaitandnotify/ProducerAndConsumer.java
+++ selfcode/thread/vwaitandnotify/ProducerAndConsumer.java
@@ -17,6 +17,16 @@ package thread.vwaitandnotify;
 public class ProducerAndConsumer {
     public static void main(String[] args) {
 
+        Clerk clerk = new Clerk();
+
+        Producer producer1 = new Producer(clerk);
+        Consumer consumer1 = new Consumer(clerk);
+//        Consumer consumer2 = new Consumer(clerk);
+
+        producer1.start();
+        consumer1.start();
+//        consumer2.start();
+
 
 
     }
