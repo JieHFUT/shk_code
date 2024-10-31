65f3b94f2ea2fd2e4b271a681d4f709371fc78e9
JieHFUT <9594523wrj@gmail.com>
2024-10-31 21:14:35 +0800

    io

---
+++ selfcode/ioandstream/file_1/FileTest.java
@@ -0,0 +1,16 @@
+package ioandstream.file_1;
+
+/**
+ * ClassName: FileTest
+ * Package: ioandstream.file_1
+ * Description:
+ *
+ * @Author jieHFUT
+ * @Create 2024/10/31 21:10
+ * @Version 1.0
+ */
+public class FileTest {
+    public static void main(String[] args) {
+        
+    }
+}
