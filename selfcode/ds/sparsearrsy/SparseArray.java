8e852c1820900d97b59bfff4ac9c6cd5cb89f490
JieHFUT <9594523wrj@gmail.com>
2024-11-14 21:08:37 +0800

    self ds review

---
+++ selfcode/ds/sparsearrsy/SparseArray.java
@@ -0,0 +1,21 @@
+package ds.sparsearrsy;
+
+/**
+ * ClassName: SparseArray
+ * Package: ds.sparsearrsy
+ * Description:
+ * 练习稀疏数组
+ * @Author jieHFUT
+ * @Create 2024/11/14 21:05
+ * @Version 1.0
+ */
+public class SparseArray {
+
+    // 创建一个原始的二维数组棋盘
+    // 0 代表没有棋子
+    // 1 代表有棋子
+    int[][] array = new int[11][11];
+    
+
+
+}
